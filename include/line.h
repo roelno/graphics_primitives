4f30994feff8f508bbcd3bbaecc0e48ef4f439cc
zelda <lichtseeker@gmail.com>
2024-05-23 21:04:08 -0700

    Check and add docs for line.h

--- include/line.h
+++ include/line.h
@@ -14,11 +14,34 @@ typedef struct {
   Point b;
 } Line;
 
+/**
+ * @brief Initialize a 2D line.
+ */
 void line_set2D(Line *l, double x0, double y0, double x1, double y1);
+
+/**
+ * @brief Initialize a line to ta and tb.
+ */
 void line_set(Line *l, Point ta, Point tb);
+
+/**
+ * @brief Set the z-buffer flag to the given value.
+ */
 void line_zBuffer(Line *l, int flag);
+
+/**
+ * @brief Normalize the x and y values of the endpoints by their homogeneous coordinate.
+ */
 void line_normalize(Line *l);
+
+/**
+ * @brief Copy the line data structure.
+ */
 void line_copy(Line *to, Line *from);
-void line_draw(Line *l, Image *src, FPixel c);
+
+/**
+ * @brief Draw the line into src using color c and the z-buffer, if appropriate.
+ */
+void line_draw(Line *l, Image *src, Color c);
 
 #endif // LINE_H
